b666ac3843ac743618aceb0283dd325af5697fb8
admin <pallavi.kelgaonkar11@gmail.com>
2019-04-27 16:20:39 +0530

    technative admin email added

--- ajaxserver/servermailchimp.php
+++ ajaxserver/servermailchimp.php
@@ -1,7 +1,7 @@
 <?php
 $response = array();
 /*
- IMPORTANT : 
+ IMPORTANT :
   - Change mailcimp API Key and List ID by your own
   - Change message from user email recipient by your own
 */
@@ -10,48 +10,48 @@ $response = array();
  */
 // check email into post data
 if (isset($_POST['submit_email'])) {
-//    $email = $_POST['email'];  
-    $email = filter_var(@$_POST['email'], FILTER_SANITIZE_EMAIL );   
+//    $email = $_POST['email'];
+    $email = filter_var(@$_POST['email'], FILTER_SANITIZE_EMAIL );
     if (!isset($response['error']) || $response['error'] === '') {
 
 //      Store email address
 //        $response = $this->storeAddress($email);
         $response = storeAddress($email);
     }
-    echo json_encode($response);    
-} 
+    echo json_encode($response);
+}
 
 /*
  * store address to mailchimp mailing list
- * IMPORTANT : 
+ * IMPORTANT :
  - Replace 'YOUR_APIKEY_HERE' by your api key from your mailchimp
      Get one here http://admin.mailchimp.com/account/api/
  - Replace 'YOUR_LISTID_HERE' by your list's unique ID
     Create a list here http://admin.mailchimp.com/lists/
-    Then Click the "settings" link for the list - the Unique Id is at the bottom of that page. 
+    Then Click the "settings" link for the list - the Unique Id is at the bottom of that page.
  */
 function storeAddress($user_email){
-    
+
     include('./mailchimp/MailChimp.php');
 
 	// IMPORTANT : grab an API Key from http://admin.mailchimp.com/account/api/ and replace YOUR_APIKEY_HERE by yours
     $MailChimp = new MailChimp('YOUR_APIKEY_HERE');
-	
+
 	//  IMPORTANT : grab your List's Unique Id by going to http://admin.mailchimp.com/lists/ and replace YOUR_LISTID_HERE by yours
-	// Click the "settings" link for the list - the Unique Id is at the bottom of that page. 
+	// Click the "settings" link for the list - the Unique Id is at the bottom of that page.
 	$list_id = "YOUR_LISTID_HERE";
 
     $m_response = array();
-    
+
 	// Validation
-	if(!$user_email){ 
+	if(!$user_email){
         $m_response['error'] = "No email address provided";
-        return $m_response; 
-    } 
+        return $m_response;
+    }
 
 	if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*$/i", $user_email)) {
         $m_response['error'] = "Email address is invalid";
-        return $m_response; 
+        return $m_response;
 	}
 
     // send mailchimp
@@ -87,10 +87,10 @@ if (isset($_POST['submit_message'])) {
     $name = trim($_POST['name']);
     $product = trim($_POST['product']);
     $message = trim($_POST['message']);
-    
-    
+
+
 	$email = filter_var(@$_POST['email'], FILTER_SANITIZE_EMAIL );
-	
+
 	$name = htmlentities($name);
 	$product = htmlentities($product);
 	$message = htmlentities($message);
@@ -110,44 +110,44 @@ if (isset($_POST['submit_message'])) {
     }
 
 	// Process to emailing if forms are correct
-    if (!isset($response['error']) || $response['error'] === '') {       
+    if (!isset($response['error']) || $response['error'] === '') {
 
-        
+
 		/* in this sample code, messages will be stored in a text file */
 //        PROCESS TO STORE MESSAGE GOES HERE
-        
+
         $content = "Name: " . $name . " \r\nEmail: " . $email .  " \r\nMessage: " . $message;
         $content = str_replace(array('<','>'),array('&lt;','&gt;'),$content);
         $name = str_replace(array('<','>'),array('&lt;','&gt;'),$name);
         $message = str_replace(array('<','>'),array('&lt;','&gt;'),$message);
-        
+
         // -- BELOW : EXAMPLE SEND YOU AN EMAIL CONTAINING THE MESSAGE (comment to disable it/ uncomment it to enable it)
         // Set the recipient email address.
         // IMPORTANT - FIXME: Update this to your desired email address (relative to your server domaine).
-        $recipient = "your@email.com";
+        $recipient = "admin@technative.in";
 
         // Set the email subject.
         $subject = "Need support message From ".$name;
 
         // Build the email content.
-        $email_content = $message."\n \n";       
+        $email_content = $message."\n \n";
         $email_content .= "Sincerely,";
         $email_content .= "From: $name\n\n";
         $email_content .= "Email: $email\n\n";
 
         // Build the email headers.
-		$email_headers = "MIME-Version: 1.0" . "\r\n"; 
-		$email_headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n"; 
+		$email_headers = "MIME-Version: 1.0" . "\r\n";
+		$email_headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
 		$email_headers .= "From: $name <$email>" . "\r\n";
 		$email_headers .= "Reply-To: <$email>";
-        
+
 
         // Send the email.
         if ( mail($recipient, $subject, $email_content, $email_headers) ) {
             // Set a 200 (okay) response code.
            	http_response_code(200);
             $response['success'] = 'Thank You! Your message has been sent';
-            
+
         } else {
             // Set a 500 (internal server error) response code.
            	http_response_code(500);
@@ -156,8 +156,8 @@ if (isset($_POST['submit_message'])) {
             // Uncomment below to Write message into a file as a backup
             //file_put_contents("message.txt", $content . "\r\n---------\r\n", FILE_APPEND | LOCK_EX);
         }
-                 
-    } 
+
+    }
 	else {
         // Set a 403 (error) forbidden response code due missing data to error.
        	http_response_code(403);
